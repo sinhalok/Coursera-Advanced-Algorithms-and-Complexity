ae66df98cfa1aae772ac03e71c7022dedd96a59d
ALOK SINHA <35992268+sinhalok@users.noreply.github.com>
2019-03-15 15:25:03 +0530

    Update evacuation.cpp

--- WEEK1/evacuation.cpp
+++ WEEK1/evacuation.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 #define arraysize 201
 int maxData = 0x7fffffff;
-int capacity[arraysize][arraysize]; //记录残留网络的容量
-int flow[arraysize];                //标记从源点到当前节点实际还剩多少流量可用
-int pre[arraysize];                 //标记在这条路径上当前节点的前驱,同时标记该节点是否在队列中
+int capacity[arraysize][arraysize]; 
+int flow[arraysize];                
+int pre[arraysize];                
 int n,m;
 queue<int> myqueue;
 int BFS(int src,int des)
 {
     int i,j;
-    while(!myqueue.empty())       //队列清空
+    while(!myqueue.empty())      
         myqueue.pop();
     for(i=1;i<m+1;++i)
     {
@@ -25,19 +25,19 @@ int BFS(int src,int des)
     {
         int index = myqueue.front();
         myqueue.pop();
-        if(index == des)            //找到了增广路径
+        if(index == des)          
             break;
         for(i=1;i<m+1;++i)
         {
             if(i!=src && capacity[index][i]>0 && pre[i]==-1)
             {
-                 pre[i] = index; //记录前驱
-                 flow[i] = min(capacity[index][i],flow[index]);   //关键：迭代的找到增量
+                 pre[i] = index; 
+                 flow[i] = min(capacity[index][i],flow[index]);   
                  myqueue.push(i);
             }
         }
     }
-    if(pre[des]==-1)      //残留图中不再存在增广路径
+    if(pre[des]==-1)      
         return -1;
     else
         return flow[des];
@@ -48,12 +48,12 @@ int maxFlow(int src,int des)
     int sumflow = 0;
     while((increasement=BFS(src,des))!=-1)
     {
-         int k = des;          //利用前驱寻找路径
+         int k = des;         
          while(k!=src)
          {
               int last = pre[k];
-              capacity[last][k] -= increasement; //改变正向边的容量
-              capacity[k][last] += increasement; //改变反向边的容量
+              capacity[last][k] -= increasement; 
+              capacity[k][last] += increasement; 
               k = last;
          }
          sumflow += increasement;
@@ -75,9 +75,9 @@ int main()
         for(i=0;i<n;++i)
         {
             cin>>start>>end>>ci;
-            if(start == end)               //考虑起点终点相同的情况
+            if(start == end)               
                continue;
-            capacity[start][end] +=ci;     //此处注意可能出现多条同一起点终点的情况
+            capacity[start][end] +=ci;     
         }
         cout<<maxFlow(1,m)<<endl;
     }
